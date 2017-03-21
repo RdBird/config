043778e2300445c662e4769048f4ab9b0d764ac6
Julien Polo <julien.polo@gmail.com>
2017-03-21 16:18:02 +0100

    Add tests for jsdoc

---
+++ __tests__/fixtures/jsdoc.js
@@ -0,0 +1,12 @@
+
+/**
+ * [run description]
+ *
+ * @param   foo - [description]
+ * @param   bar - [description]
+ * @param   baz - [description]
+ * @returns       [description]
+ */
+export function run(foo, bar, baz) {
+
+}
