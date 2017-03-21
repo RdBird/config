a2c4d617f52fb3eec2384262e35c096bf6bf9f7f
Julien Polo <julien.polo@gmail.com>
2017-03-21 16:06:56 +0100

    Add jsdoc rules

--- rules/jsdoc/jsdoc.js
+++ rules/jsdoc/jsdoc.js
@@ -1,5 +1,15 @@
 module.exports = {
   rules: {
-
+    'jsdoc/check-param-names': 'error',
+    'jsdoc/check-tag-names': 'error',
+    'jsdoc/check-types': 'error',
+    'jsdoc/newline-after-description': 'error',
+    'jsdoc/require-description-complete-sentence': 0,
+    'jsdoc/require-hyphen-before-param-description': 'error',
+    'jsdoc/require-param-description': 'error',
+    'jsdoc/require-param-type': 0,
+    'jsdoc/require-param': 0,
+    'jsdoc/require-returns-description': 'error',
+    'jsdoc/require-returns-type': 0,
   },
 };
